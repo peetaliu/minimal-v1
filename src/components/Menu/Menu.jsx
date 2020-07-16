435d9df4e219761cb144258290053e7d29026be4
Peter <peterdotliu@gmail.com>
2020-07-16 00:30:18 -0400

    Add logo and style to Menu component

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import PortfolioContext from '../../context/context';
 
-const Menu = ({ ...props }) => {
+const Menu = () => {
+  const [scroll, setScroll] = useState(0);
+  const { menu } = useContext(PortfolioContext);
+  const { logo, items } = menu;
+
+  onscroll = () => {
+    setScroll(window.pageYOffset);
+    console.log(scroll);
+  };
+
   return (
-    <div id="menu">
-      <ul></ul>
+    <div id={scroll > 50 ? 'menu__scrolled' : 'menu__top'}>
+      <h1 className="logo">{logo}</h1>
+      {items ? (
+        <ul className="items">
+          {items.map((i) => (
+            <li key={i.id}>
+              <i className={`fa fa-${i.name} fa-outline`} />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        ''
+      )}
     </div>
   );
 };
