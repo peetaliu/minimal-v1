3e262f9327ace4d567e74b02ba1e21297e1d81cc
Peter <peterdotliu@gmail.com>
2020-07-24 20:06:48 -0400

    Add background image for hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,6 +3,7 @@ import { Container } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-scroll';
 import PortfolioContext from '../../context/context';
+import bg from '../../images/bg.jpg';
 
 const Header = () => {
   const { hero } = useContext(PortfolioContext);
