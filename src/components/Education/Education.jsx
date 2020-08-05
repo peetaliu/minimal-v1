d69642e0d59b3123ac96a1228875f77acabeb307
Peter <peterdotliu@gmail.com>
2020-08-05 19:18:58 -0400

    Add fade in effect

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,9 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import PortfolioContext from '../../context/context';
+import Fade from 'react-reveal/Fade';
 
 const Education = () => {
   const { edu } = useContext(PortfolioContext);
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
 
   return (
     <div id="edu">
@@ -15,12 +28,21 @@ const Education = () => {
         <Row xs={1}>
           {edu.map((e, i) => {
             return (
-              <Col className={i < edu.length - 1 ? 'eduEntry' : 'lastEntry'}>
-                <span className="eduDate">{e.certDate}</span>
-                <h3>{e.cert}</h3>
-                <h3>{e.school}</h3>
-                <p>{e.program}</p>
-              </Col>
+              <Fade
+                left={isDesktop}
+                bottom={isMobile}
+                duration={750}
+                delay={300}
+                distance="30px"
+                key={e.cert}
+              >
+                <Col className={i < edu.length - 1 ? 'eduEntry' : 'lastEntry'}>
+                  <span className="eduDate">{e.certDate}</span>
+                  <h3>{e.cert}</h3>
+                  <h3>{e.school}</h3>
+                  <p>{e.program}</p>
+                </Col>
+              </Fade>
             );
           })}
         </Row>
