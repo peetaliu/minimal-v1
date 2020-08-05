2a056ed3023d958f00f8e1c530979cb31c4c3efe
Peter <peterdotliu@gmail.com>
2020-08-05 19:20:36 -0400

    Remove dark mode button, Change logo font

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,7 +3,7 @@ import PortfolioContext from '../../context/context';
 
 const Menu = () => {
   // const [scroll, setScroll] = useState(0);
-  const [darkMode, setDarkMode] = useState(false);
+  // const [darkMode, setDarkMode] = useState(false);
   const { menu } = useContext(PortfolioContext);
   const { logo } = menu;
 
@@ -11,18 +11,19 @@ const Menu = () => {
   //   setScroll(window.pageYOffset);
   // };
 
-  const handleDark = () => {
-    setDarkMode(!darkMode);
-  };
+  // const handleDark = () => {
+  //   setDarkMode(!darkMode);
+  // };
 
   return (
     <div id="menu__top">
       <h1 className="logo">{logo}</h1>
+      {/* Darkmode TBC
       <div className="items">
         <button id="drkModeBtn" onClick={handleDark}>
           <i className={`fa fa-lightbulb fa-outline`} />
         </button>
-      </div>
+      </div> */}
     </div>
   );
 };
