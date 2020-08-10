ad0334f6f62d3f344965ccdda1a386fe41e006b0
Peter <peterdotliu@gmail.com>
2020-08-09 22:13:36 -0400

    Update about data

--- src/mock/data.js
+++ src/mock/data.js
@@ -27,11 +27,11 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   paragraphOne:
-    "I love programming, travelling, photography, cars, and lifting (somewhat) heavy things. My preferred dev stack is a React frontend with a Node/Express backend and either MongoDB or PostgreSQL for storage. I've built REST api's but have also dabbled a bit with GraphQL and I'm excited to put what I've learned into practice.",
+    "Greetings! how nice of you to stop by. So you want to know a bit about me. Well, to sum it up, I love coding, travelling, photography, cars, and lifting (somewhat) heavy things. Pretty basic eh? Anyways, you're probably here for the techy stuff so let's dive right in. My preferred dev stack is a React frontend with a Node/Express backend and either MongoDB or PostgreSQL for storage. I've built REST api's in my personal projects and also dabbled a bit with GraphQL. While this is my preferred stack, it's not the only one I'll work with. I'm always willing to learn something new!",
   paragraphTwo:
     "My previous non-tech related roles include Project Coordinator for an ad agency and also a personal trainer (which I still do from time-to-time). However, don't let my previous occupations fool you, Web development has always been my passion.",
   paragraphThree:
-    "When I'm not working on dev stuff, you'll usually find me in the gym, in the garage working on my car, or planning my next big trip!",
+    "When I'm not working on dev stuff, you'll usually find me in the gym, in the garage working on my car, or getting lost in some foreign country.",
   resume: '',
 };
 
